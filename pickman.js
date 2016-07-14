899969d1f5669ed6c25245fd570159a0c9f0ef52
s15359ys <s15359ys@sfc.keio.ac.jp>
2016-07-14 14:35:55 +0900

    注意書きちょっと直した

--- pickman.js
+++ pickman.js
@@ -4,7 +4,7 @@ function ShowSelectedData(){
         //BGMを流す
         document.write('<audio id="Name" preload="auto"><source src="audio/01.mp3" type="audio/mp3"></audio>') ;
         document.write('<script>document.getElementById("Name").play();</script> ');
-        document.write('※このページは開いた瞬間に客入れ音楽が流れます')
+        document.write('※このページは開いた瞬間に客入れ音楽が流れます<br>')
         //フライヤーを載せる
         document.write('<img src="01.jpg" width="500" height="700"><br>');
         //公演情報
