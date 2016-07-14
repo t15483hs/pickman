58575d19ac7df2561b1fa06de7818a654c92d79d
s15359ys <s15359ys@sfc.keio.ac.jp>
2016-07-14 11:20:52 +0900

    2016/07/14 11:19
    トップページをそれっぽく
    別れの曲のスタッフ欄を追記
    jsのdocument.write欄を改行しても読めるように力技を行使

--- pickman.js
+++ pickman.js
@@ -3,11 +3,36 @@ function ShowSelectedData(){
     if(show==1){
         document.write('<img src="1.jpg" width="500" height="700"><br>');
         document.write('<h3>◇あらすじ</h3><br>');
-        document.write('<h4>人気のない惑星のお屋敷に<br>女の子と二人の従者だけが<br>暮らしておりました。<br>ほかに誰かやってきても<br>みんなお嬢様に追い出されてしまうのです。<br>あぁ<br>噂をすれば<br>それでは失礼させていただきます<br>お客様をお待たせするわけにはいきませんからね。</h4><br><h3>◇原案</h3><br><h4>瀧岡美咲</h4><br><h3>◇脚本</h3><br><h4>白石響</h4><br><h3>◇演出</h3><br><h4>頃末龍</h4><br><h3>◇出演</h3><br><h4>境大軌・村上航・本岡万侑・山本静香</h4></br><h3>◇スタッフ</h3><h4>舞台監督　 瀧岡美咲<br>舞台美術 　加藤真央・尾林太郎・木村智矩・木村美帆・瀧岡美咲・野宮傑<br>音響 　齋藤大樹・境大軌・野宮傑<br>照明　 山本希<br>宣伝美術 　頃末龍<br>衣裳　栗原朋子<br>広報　 山本希<br>制作　 白石響<br></h4>' );
+        document.write('<h4>人気のない惑星のお屋敷に<br>');
+        document.write('女の子と二人の従者だけが<br>');
+        document.write('暮らしておりました。<br>');
+        document.write('ほかに誰かやってきても<br>')
+        document.write('みんなお嬢様に追い出されてしまうのです。<br>あぁ<br>噂をすれば <br>')
+        document.write('それでは失礼させていただきます<br>')
+        document.write('お客様をお待たせするわけにはいきませんからね。</h4><br>');
+        document.write('<h3>◇原案</h3><br><h4>瀧岡美咲</h4><br>');
+        document.write('<h3>◇脚本</h3><br><h4>白石響</h4><br>');
+        document.write('<h3>◇演出</h3><br><h4>頃末龍</h4><br>');
+        document.write('<h3>◇出演</h3><br><h4>境大軌・村上航・本岡万侑・山本静香</h4></br>')
+        document.write('<h3>◇スタッフ</h3><h4>舞台監督　 瀧岡美咲<br>');
+        document.write('舞台美術 　加藤真央・尾林太郎・木村智矩・木村美帆・瀧岡美咲・野宮傑<br>')
+        document.write('音響 　齋藤大樹・境大軌・野宮傑<br>照明　 山本希<br>');
+        document.write('宣伝美術 　頃末龍<br>衣裳　栗原朋子<br>広報　 山本希<br>制作　 白石響<br></h4>' );
     }
     else if(show==2){
-        document.write('<img src="2.jpg" width="500" height="700"><h3>◇あらすじ</h3><br><h4><子供の頃からの親友だったぬいぐるみを母に捨てられてしまったハルマは友達のいない高校二年生。母を心配させないため、日々友達を作ろうと試行錯誤するハルマの前にある日謎の男が現れてこう言った。「君の願いを叶えてあげる」そこから始まるハルマの短い夏の物語。<br></h4><br><h3>◇脚本・演出</h3><br><h4>赤間洸介</h4><br><h3>◇出演</h3><br><h4>岩城智子・木村智矩・木村美帆・熊谷啓孝<br>斎藤悠人・田村重人・星野礼馬・吉井優香</h4><br><h3>◇スタッフ</h3><br><h4>舞台監督　 瀧岡美咲<br>舞台美術 　加藤真央・尾林太郎・木村智矩・木村美帆・瀧岡美咲・野宮傑<br>音響 　齋藤大樹・境大軌・野宮傑<br>照明　 山本希<br>宣伝美術 　頃末龍<br>衣裳　栗原朋子<br>広報　 山本希<br>制作　 白石響<br></h4>' );
-    }
+        document.write('<img src="2.jpg" width="500" height="700"><br>');
+        document.write('<h3>◇あらすじ</h3><br>');
+        document.write('<h4><子供の頃からの親友だったぬいぐるみを母に捨てられてしまったハルマは友達のいない高校二年生。<br>');
+        document.write('母を心配させないため、日々友達を作ろうと試行錯誤するハルマの前にある日謎の男が現れてこう言った。「君の願いを叶えてあげる」<br>')
+        document.write('そこから始まるハルマの短い夏の物語。</h4><br>');
+        document.write('<h3>◇脚本・演出</h3><br><h4>赤間洸介</h4><br>');
+        document.write('<h3>◇出演</h3><br><h4>岩城智子・木村智矩・木村美帆・熊谷啓孝<br>');
+        document.write('斎藤悠人・田村重人・星野礼馬・吉井優香</h4><br>');
+        document.write('<h3>◇スタッフ</h3><h4>舞台監督　 瀧岡美咲<br>');
+        document.write('舞台美術 　加藤真央・尾林太郎・木村智矩・木村美帆・瀧岡美咲・野宮傑<br>')
+        document.write('音響 　齋藤大樹・境大軌・野宮傑<br>照明　 山本希<br>');
+        document.write('宣伝美術 　頃末龍<br>衣裳　栗原朋子<br>広報　 山本希<br>制作　 白石響<br></h4>' );
+        }
     else if(show==3){
         document.write('<img src="3.jpg" width="500" height="700">');
     }
