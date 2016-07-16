8774180841415137b0ff86afa98aaf633bcba36f
s15359ys <s15359ys@sfc.keio.ac.jp>
2016-07-16 17:25:41 +0900

    公演を年度ごとにわけられるようにしたよ

--- pickman_genuine/pickman_genuine.js
+++ pickman_genuine/pickman_genuine.js
@@ -1,6 +1,11 @@
 function ShowSelectedData(){
     var show = document.database.play.value;
-    location.href="plays/"+show+".html";
+    var year = show[0]+show[1]+show[2]+show[3]
+    if(show=="00"){
+      alert("閲覧したい過去公演を選択してください")
+    }else{
+    location.href="plays/"+year+"/"+show+".html";
+    }
     }
 function stop(){
   var audio;
